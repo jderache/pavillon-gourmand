93d6d061fe4f0f49d4e25d251b478da356c3af9e
Julien DERACHE <julienderache.pro@gmail.com>
2024-05-21 14:05:52 +0200

    add footer.php

---
+++ parts/footer.php
@@ -0,0 +1,12 @@
+<footer>
+  <div class="distinctions">
+    <img src="./public/images/michelin.svg" alt="3 étoiles Michelin - 2023" />
+    <img src="./public/images/gault-millau.svg" alt="Gault et Millau" />
+    <img src="./public/images/relais-chateaux.svg" alt="Relais & Chateaux" />
+    <img src="./public/images/ecotable.svg" alt="Écotable" />
+  </div>
+  <div class="footer-bottom">
+    <p>Le Pavillon Gourmand</p>
+    <a href="#">Mentions légales</a>
+  </div>
+</footer>
